c6493a33ce50a06e05e1d3e3470879ede7b8fc26
Ankesh-raj <ankeshraj1702@gmail.com>
2024-10-11 01:04:25 +0530

    I have comitted lab 4

---
+++ src/Octoberex0410/Lab004.java
@@ -0,0 +1,8 @@
+package Octoberex0410;
+
+public class Lab004 {
+   public static void main(String[] args) {
+
+       System.out.printf("Hello and welcome!");
+   }
+}
--- src/Octoberex10210/Lab001.java
+++ src/Octoberex10210/Lab001.java
@@ -1,4 +1,9 @@
 package Octoberex10210;
 
-public class Lab001 {
+public class Lab001
+{
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+    }
 }
